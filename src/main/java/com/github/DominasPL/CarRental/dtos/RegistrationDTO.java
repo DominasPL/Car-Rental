bb6cd42b9f236e2aebf15fcb34825efb7cb6444e
DomiasPL <dominik-stepuch@wp.pl>
2019-06-20 17:52:43 +0200

    Added username and password validation to registration form

--- src/main/java/com/github/DominasPL/CarRental/dtos/RegistrationDTO.java
+++ src/main/java/com/github/DominasPL/CarRental/dtos/RegistrationDTO.java
@@ -5,15 +5,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegistrationDTO {
 
+    @Size(min = 3, max = 20)
+    @NotBlank
     private String username;
 
+    @Size(min = 8, max = 30)
+    @NotBlank
     private String password;
 
+    @Size(min = 8, max = 30)
+    @NotBlank
     private String confirmedPassword;
 
+    //TODO DODAC REGEXP DO PASSWORD
 }
