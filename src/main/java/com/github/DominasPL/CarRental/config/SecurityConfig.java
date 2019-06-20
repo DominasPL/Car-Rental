167a1d2d21e6cdfeddeb0a9dcb6b15582faee80d
DomiasPL <dominik-stepuch@wp.pl>
2019-06-20 16:14:02 +0200

    Added application.properties, security config and User entity

---
+++ src/main/java/com/github/DominasPL/CarRental/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.github.DominasPL.CarRental.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT email, password, true FROM users WHERE email = ?")
+                .authoritiesByUsernameQuery("SELECT username, ROLE_USER FROM users WHERE username = ?");
+//                .authoritiesByUsernameQuery("SELECT email, role FROM users\n" +
+//                        "INNER JOIN users_roles ON users.id = users_roles.user_id\n" +
+//                        "INNER JOIN roles ON users_roles.role_id = roles.id\n" +
+//                        "WHERE users.email = ?;");
+
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                //tak jak w bolku try catch od najbardziej szczegółowych do najmniej
+                .antMatchers("/register").anonymous() // dostep dla niezalogowanych
+                .antMatchers("/register/confirm-account").anonymous()
+                .antMatchers("/login").anonymous() // dostep dla niezalogowanych
+                .antMatchers("/password-reset").anonymous() // dostep dla niezalogowanych
+                .antMatchers("/password-reset/reset").anonymous() // dostep dla niezalogowanych
+                .antMatchers("/about-service").permitAll() // dostep dla wszystkich
+                .antMatchers("/about-us").permitAll() // dostep dla wszystkich
+                .antMatchers("/organizations").permitAll() // dostep dla wszystkich
+                .antMatchers("/contact").permitAll() // dostep dla wszystkich
+                .antMatchers("/").permitAll() // dostep dla wszystkich
+//                .antMatchers("/user", "/user/**").hasRole("USER") // tylko dla userow
+                .antMatchers("/admin", "/admin/**").hasRole("ADMIN") // tylko dla adminow
+                .antMatchers("/media/**").permitAll()//dostep do folderu media dla wszystkich
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/check-role") // domyślna strona po zalogowaniu
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/") // strona po wylogowaniu
+                .and()
+                .csrf()
+                .disable();
+
+    }
+}
---
+++ src/main/java/com/github/DominasPL/CarRental/domain/entities/User.java
@@ -0,0 +1,27 @@
+package com.github.DominasPL.CarRental.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+
+}
