110a1d064940c46a366ee10cc981a7189220db3d
DomiasPL <dominik-stepuch@wp.pl>
2019-06-29 20:29:39 +0200

    Added rental entity

---
+++ src/main/java/com/github/DominasPL/CarRental/domain/entities/Rental.java
@@ -0,0 +1,29 @@
+package com.github.DominasPL.CarRental.domain.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rentals")
+public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String hirePlace;
+
+    @Column(nullable = false)
+    private String returnPlace;
+
+    @Column(nullable = false)
+    private String hireDate;
+
+    @Column(nullable = false)
+    private String returnDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
